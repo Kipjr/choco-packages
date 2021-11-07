1df55505eda1f50d17f248cb7f28bfbeae0ce4e6
Kipjr <martijn9873@hotmail.com>
2021-11-07 14:29:09 +0100

    Add: MQTT-Explorer

---
+++ mqtt-explorer/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/thomasnordquist/MQTT-Explorer/releases/download/0.0.0-0.4.0-beta1/MQTT-Explorer-Setup-0.4.0-beta1.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'MQTT Explorer 0.4.0-beta1'
+  checksum      = '6DADADA22FFA172DE337CA5F2C8FFDF2887B104A65DC1E06D557811D1D8448ED'
+  checksumType  = 'sha256'
+  validExitCodes= @(0)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
