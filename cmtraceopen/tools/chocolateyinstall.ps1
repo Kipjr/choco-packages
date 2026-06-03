d676b2b4be1882f2b3d3b9c8e54b9ac3b8889a0a
Martijn <12066560+Kipjr@users.noreply.github.com>
2026-06-03 11:49:54 +0200

    Create chocolateyinstall.ps1

---
+++ cmtraceopen/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/adamgell/cmtraceopen/releases/download/v1.3.2/CMTrace-Open_1.3.2_x64.msi'
+
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  softwareName  = 'CMTrace Open'
+  checksum      = ''
+  checksumType  = 'sha256'
+  validExitCodes= @(0)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
