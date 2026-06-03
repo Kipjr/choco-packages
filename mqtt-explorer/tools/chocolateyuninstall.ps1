10d8e7d712078be3839c0de5b198f04809ad69e6
Martijn <12066560+Kipjr@users.noreply.github.com>
2026-06-03 13:32:01 +0200

    Fix errors (#15)
    * Update chocolateyinstall.ps1
    * Update chocolateyuninstall.ps1

--- mqtt-explorer/tools/chocolateyuninstall.ps1
+++ mqtt-explorer/tools/chocolateyuninstall.ps1
@@ -14,6 +14,7 @@ if ($key.Count -eq 1) {
   $key | ForEach-Object { 
     $packageArgs['file'] = "$(($_.UninstallString | select-string -Pattern '"(.*?)"').matches.Value.trim('"'))"
     Uninstall-ChocolateyPackage @packageArgs
+    $uninstalled = $true
   }
 } elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
@@ -21,5 +22,5 @@ if ($key.Count -eq 1) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
+  $key | Foreach-Object {Write-Warning "- $($_.DisplayName)"}
 }
